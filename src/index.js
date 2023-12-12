c174391dd636d50c30d949e37179347f7e35ae49
Riley Seaburg <riley@rileyseaburg.com>
2023-12-11 21:13:24 -0500

    Refactor GrapesJS initialization and add error handling

--- src/index.js
+++ src/index.js
@@ -6,59 +6,54 @@ import tailwindCss from '@rustyroad/editor';
 import projectManager from 'grapesjs-project-manager';
 import codeEditor from 'grapesjs-component-code-editor';
 import scriptEditor from 'grapesjs-script-editor';
+// Get the container element
+var container = document.getElementById('gjs');
 
-
-window.editor = GrapesJS.init({
-  container: '#gjs',
-  height: '100%',
-  fromElement: true,
-  storageManager: false,
-  
-  showOffsets: true,
-  fromElement: true,
-  noticeOnUnload: false,
-  plugins: [
-    imageEditorPlugin,
-    grapesWebpagePreset,
-    grapesjsBlocksBasic,
-    tailwindCss,
-    codeEditor,
-    scriptEditor
-  ],
-  pluginsOpts: {
-    [imageEditorPlugin]: {
-      // Plugin-specific options for grapesjs-tui-image-editor
-    },
-    [grapesWebpagePreset]: {
-      // Plugin-specific options for grapesjs-preset-webpage
-    },
-    [grapesjsBlocksBasic]: {
-      // Plugin-specific options for grapesjs-blocks-basic
-    },
-    [tailwindCss]: {
-      // Plugin-specific options for grapesjs-blocks-basic
-    },
- 
-    [codeEditor]: {
-      // Plugin-specific options for grapesjs-blocks-basic
-    },
-    [scriptEditor]: {
-      // Plugin-specific options for grapesjs-blocks-basic
-    }
-  }
-
-
-
-});
-
-
-alert(fetch('https://jsonplaceholder.typicode.com/todos/1 ')
-  .then(response => response.json())
-  .then(json => {
-    console.log(json);
-  })
-  .catch(err => {
-    console.log(err);
-  })
-
-)
+// Check if the container exists
+if (container) {
+    // Initialize GrapesJS here
+    window.editor = GrapesJS.init({
+      container: '#gjs',
+      height: '100%',
+      fromElement: true,
+      storageManager: false,
+      
+      showOffsets: true,
+      fromElement: true,
+      noticeOnUnload: false,
+      plugins: [
+        imageEditorPlugin,
+        grapesWebpagePreset,
+        grapesjsBlocksBasic,
+        tailwindCss,
+        codeEditor,
+        scriptEditor
+      ],
+      pluginsOpts: {
+        [imageEditorPlugin]: {
+          // Plugin-specific options for grapesjs-tui-image-editor
+        },
+        [grapesWebpagePreset]: {
+          // Plugin-specific options for grapesjs-preset-webpage
+        },
+        [grapesjsBlocksBasic]: {
+          // Plugin-specific options for grapesjs-blocks-basic
+        },
+        [tailwindCss]: {
+          // Plugin-specific options for grapesjs-blocks-basic
+        },
+     
+        [codeEditor]: {
+          // Plugin-specific options for grapesjs-blocks-basic
+        },
+        [scriptEditor]: {
+          // Plugin-specific options for grapesjs-blocks-basic
+        }
+      }
+    
+    
+    
+    });
+} else {
+    console.error('The GrapesJS container does not exist in the DOM.');
+}
